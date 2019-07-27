28dfbcd0a59822a76d63d13027b9c3bee6f03244
むずでょ <muzudho1@gmail.com>
2019-07-27 15:04:06 +0900

    ブロックの切れ目を連続しないように変更。

--- evaluation.py
+++ evaluation.py
@@ -1,4 +1,13 @@
 def evaluate(pos_df):
+    """
+    position.csv
+    ------------
+
+X,Y,BLOCK,TABLE,PARTICIPANT,GENRE_CODE
+0,0,C,27,2,Red
+1,0,C,26,1,Red
+2,0,C,25,37,Red
+    """
 
     # 評価値
     value = 0
@@ -29,20 +38,24 @@ def evaluate(pos_df):
             break
 
     # 集計。テーブル番号順にして、同じ色が連続したら、連続した数だけ加点。
+    # ただし、ブロックの切れ目は連続しない。
     continue_bonus = 0
     sorted_pos_df = pos_df.sort_values(by=["TABLE"], ascending=True)
     # print(sorted_pos_df.head(5))
-    table_ordered_list = sorted_pos_df[["TABLE", "GENRE_CODE"]].values.tolist()
+    table_ordered_list = sorted_pos_df[[
+        "TABLE", "BLOCK", "GENRE_CODE"]].values.tolist()
     # print("table_ordered_list: {}".format(table_ordered_list))
+    prev_block = None
     prev_genre_code = None
     for entry in table_ordered_list:
-        if prev_genre_code == entry[1]:
+        if prev_genre_code == entry[2] and prev_block == entry[1]:
             continue_bonus += 1
             value += continue_bonus
-            # print("prev_genre_code: {}, entry[1]: {}, value: {}".format(
-            #    prev_genre_code, entry[1], value))
+            # print("prev_genre_code: {}, entry[2]: {}, value: {}".format(
+            #    prev_genre_code, entry[2], value))
         else:
-            prev_genre_code = entry[1]
+            prev_genre_code = entry[2]
             continue_bonus = 0
+        prev_block = entry[1]
 
     return value
